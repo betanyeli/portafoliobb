3e54a9d0065a19c3b44038afab00d0a14c2b9d64
betanyeli <betangelii@gmail.com>
2019-08-02 00:58:46 -0400

    Creating first template portfolio node

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import photo from'./beta.jpg'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App row">
+      
+<div className="terminal col-12">
+<img src={photo} alt="betanyeli"></img>
+<br></br>
+    <span className="text-one">>Hola, Soy Betanyeli Bravo! </span>
+    <br></br>
+    <span className="text-two">>I'm a web developer! </span>
+    <br></br>
+    <span className="text-three">>Soy Desarrolladora Web! </span>
+    <p className="description">Soy Betányeli. Desde que me titulé he buscado instancias donde desarrollar nuevos desafíos, y egresar de Laboratoria y conocer el mundo de la programación, fue uno de los más gratificantes retos. Constante, empática y altruista, trabajando constantemente para el logro de mis metas.</p>
+
+    
+</div>
+    </div>
+  );
+}
+
+export default App;
