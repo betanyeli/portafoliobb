5c86e2944dbf75ae649e0c529f41ed03f10d14ea
betanyeli <betangelii@gmail.com>
2019-08-02 10:03:52 -0400

    Updates

---
+++ precache-manifest.191b2062dfd05d028c63c4264b8e2759.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea6e8a096c819a60933bb845a87d374a",
+    "url": "/portafoliobb/index.html"
+  },
+  {
+    "revision": "b34060e7da871127ba1c",
+    "url": "/portafoliobb/static/css/main.0539382c.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/portafoliobb/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "b34060e7da871127ba1c",
+    "url": "/portafoliobb/static/js/main.5ab01362.chunk.js"
+  },
+  {
+    "revision": "0adddcda29338be70f4f",
+    "url": "/portafoliobb/static/js/runtime~main.4b1f33b1.js"
+  },
+  {
+    "revision": "b44d74334a01bc6daa3edba0eb9f6da6",
+    "url": "/portafoliobb/static/media/beta.b44d7433.jpg"
+  }
+]);
