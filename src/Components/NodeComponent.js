27cd32ed15df2d25a21d3c7c573bca42e09f77dc
betanyeli <betangelii@gmail.com>
2019-09-02 13:53:07 -0400

    lasting changes before ...

--- src/Components/NodeComponent.js
+++ src/Components/NodeComponent.js
@@ -44,7 +44,7 @@ function NodeTerminal() {
                 <a className="social" href="https://www.linkedin.com/in/betanyeli-bravo/"><span>LinkedIn</span></a>
                 <a className="social" href="https://github.com/betanyeli"><span>Github</span></a>
                 <a className="social" href="mailto:betangelii@gmail.com"><span>E-mail</span></a>
-                <a className="social" href="#"><span>CV</span></a>
+                <a className="social" href="https://github.com/betanyeli/portafoliobb/blob/master/src/cv.pdf"><span>CV</span></a>
                 <a className="social" href="tel:+56972801296"><span>Tel</span></a>
                 <a className="social" href="https://www.npmjs.com/~betanyeli"><span>Npm</span></a>
               </div>
