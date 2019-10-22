c45a6870bb4f197eb2e3f1f6b10b67f375138b9d
betanyeli <betangelii@gmail.com>
2019-10-22 05:15:13 -0300

    Updates

---
+++ precache-manifest.b34b1cbda2bed609b71878408355ac5c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c503f863010f30e703f06acaa2a42b8",
+    "url": "/portafoliobb/index.html"
+  },
+  {
+    "revision": "84a5732693652c90e48b",
+    "url": "/portafoliobb/static/css/main.530c5da5.chunk.css"
+  },
+  {
+    "revision": "7a6129bba939229c1b31",
+    "url": "/portafoliobb/static/js/2.54ae3261.chunk.js"
+  },
+  {
+    "revision": "84a5732693652c90e48b",
+    "url": "/portafoliobb/static/js/main.23025d27.chunk.js"
+  },
+  {
+    "revision": "0adddcda29338be70f4f",
+    "url": "/portafoliobb/static/js/runtime~main.4b1f33b1.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portafoliobb/precache-manifest.5ead23d4dae1e4be09f7fc59771ee28e.js"
+  "/portafoliobb/precache-manifest.b34b1cbda2bed609b71878408355ac5c.js"
 );
 
 self.addEventListener('message', (event) => {
